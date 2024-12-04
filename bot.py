8fc76a7bdf7547e4d9986ea7e805acb13439fdf0
FakeQYouQ <fakeyou@internet.ru>
2024-12-04 22:49:24 +0300

    Исправлен импорт ParseMode для aiogram 3.x

--- bot.py
+++ bot.py
@@ -1,23 +1,23 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
-from aiogram.utils import executor
+from aiogram.filters import Command
 
 API_TOKEN = '7390881267:AAG1t89iR5VM9j1TAHBXiey'  # Убедитесь, что токен правильный
 
 bot = Bot(token=API_TOKEN, parse_mode="HTML")
 dp = Dispatcher()
 
-@dp.message_handler(commands=['start'])
+@dp.message(Command('start'))
 async def send_welcome(message: Message):
     await message.answer("Добро пожаловать в музыкального бота! Отправьте команду /play, чтобы начать.")
 
-@dp.message_handler(commands=['play'])
+@dp.message(Command('play'))
 async def play_music(message: Message):
     await message.answer("Вот ваш трек: /music_link")
 
 async def main():
-    await dp.start_polling()
+    await dp.start_polling()  # Запускаем polling без использования executor
 
 if __name__ == '__main__':
     asyncio.run(main())
