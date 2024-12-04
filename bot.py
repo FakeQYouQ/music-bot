bf77319c20196bd81b42434c29ec5b18da0c5fba
FakeQYouQ <fakeyou@internet.ru>
2024-12-04 21:56:12 +0300

    Исправлен импорт ParseMode для aiogram 3.x

--- bot.py
+++ bot.py
@@ -1,13 +1,10 @@
-import logging
 from aiogram import Bot, Dispatcher
-from aiogram.types import ParseMode
 from aiogram.types import Message
+from aiogram.utils import executor
 
 API_TOKEN = '7390881267:AAG1t89iR5VM9j1TAHBXieyWu9S4nvtQDzg'
 
-logging.basicConfig(level=logging.INFO)
-
-bot = Bot(token=API_TOKEN, parse_mode=ParseMode.HTML)
+bot = Bot(token=API_TOKEN, parse_mode="HTML")
 dp = Dispatcher()
 
 @dp.message(commands=['start'])
@@ -18,12 +15,5 @@ async def send_welcome(message: Message):
 async def play_music(message: Message):
     await message.answer("Вот ваш трек: /music_link")
 
-async def main():
-    try:
-        await dp.start_polling(bot)
-    finally:
-        await bot.session.close()
-
 if __name__ == '__main__':
-    import asyncio
-    asyncio.run(main())
+    executor.run(dp)
