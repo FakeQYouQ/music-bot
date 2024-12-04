1c969d8909a7bf211e462335bb249814b600b809
FakeQYouQ <fakeyou@internet.ru>
2024-12-04 21:14:36 +0300

    Added music player interface with tracks

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-function playAudio() {
-  var audio = document.getElementById("audioPlayer");
-  audio.play();
+const tracks = [
+    { name: "Aikko Prinjat Sebja", file: "aikko-prinjat-sebja.mp3" },
+    { name: "Track 2", file: "track2.mp3" },
+    { name: "Track 3", file: "track3.mp3" }
+];
+
+function loadTracks() {
+    const trackListDiv = document.getElementById("trackList");
+    tracks.forEach(track => {
+        const trackButton = document.createElement("button");
+        trackButton.textContent = track.name;
+        trackButton.addEventListener("click", () => playTrack(track.file));
+        trackListDiv.appendChild(trackButton);
+    });
 }
 
-function pauseAudio() {
-  var audio = document.getElementById("audioPlayer");
-  audio.pause();
+function playTrack(trackFile) {
+    const audioPlayer = document.getElementById("audioPlayer");
+    audioPlayer.src = trackFile;
+    audioPlayer.play();
 }
+
+window.onload = loadTracks;
