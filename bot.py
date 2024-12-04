28323656e5be004675958683275a3a69c8933bed
FakeQYouQ <fakeyou@internet.ru>
2024-12-04 23:14:24 +0300

    Сообщение коммита

--- bot.py
+++ bot.py
@@ -1,46 +1,18 @@
-import logging
-from aiogram import Bot, Dispatcher, types
-from aiogram.types import ParseMode
-from aiogram.utils import executor
-import asyncio
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 
-API_TOKEN = '7390881267:AAG1t89iR5VM9j1TAHBXieyWu9S4nvtQDzg'
+def start(update: Update, context):
+    # Кнопка для запуска мини-приложения
+    keyboard = [[InlineKeyboardButton("Слушать музыку", url="https://yourappurl.com")]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text("Нажмите на кнопку, чтобы послушать музыку.", reply_markup=reply_markup)
 
-# Настройка логирования
-logging.basicConfig(level=logging.INFO)
+def main():
+    updater = Updater("7390881267:AAG1t89iR5VM9j1TAHBXieyWu9S4nvtQDzg", use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    updater.start_polling()
+    updater.idle()
 
-# Инициализация бота
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
-
-# Команда /start
-@dp.message_handler(commands=['start'])
-async def send_welcome(message: types.Message):
-    await message.reply("Добро пожаловать в музыкальный бот! Используйте команду /play для начала.")
-
-# Команда /play для начала воспроизведения музыки
-@dp.message_handler(commands=['play'])
-async def play_music(message: types.Message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton("Следующий трек")
-    item2 = types.KeyboardButton("Пауза")
-    item3 = types.KeyboardButton("Возобновить")
-    markup.add(item1, item2, item3)
-    await message.answer("Выберите действие:", reply_markup=markup)
-
-# Обработка нажатий кнопок
-@dp.message_handler(lambda message: message.text in ["Следующий трек", "Пауза", "Возобновить"])
-async def music_controls(message: types.Message):
-    if message.text == "Следующий трек":
-        await message.answer("Воспроизводится следующий трек...")
-        # Реализуйте логику для смены трека
-    elif message.text == "Пауза":
-        await message.answer("Воспроизведение приостановлено.")
-        # Реализуйте логику для паузы
-    elif message.text == "Возобновить":
-        await message.answer("Воспроизведение продолжено.")
-        # Реализуйте логику для продолжения воспроизведения
-
-# Запуск бота
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    main()
