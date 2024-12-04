50ad2721e3271afaaf77274dfd99015af323a1ca
FakeQYouQ <fakeyou@internet.ru>
2024-12-04 23:17:25 +0300

    Сообщение коммита

--- bot.py
+++ bot.py
@@ -1,18 +1,16 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
-def start(update: Update, context):
-    # Кнопка для запуска мини-приложения
-    keyboard = [[InlineKeyboardButton("Слушать музыку", url="https://yourappurl.com")]]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text("Нажмите на кнопку, чтобы послушать музыку.", reply_markup=reply_markup)
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("Привет!")
 
-def main():
-    updater = Updater("7390881267:AAG1t89iR5VM9j1TAHBXieyWu9S4nvtQDzg", use_context=True)
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
-    updater.start_polling()
-    updater.idle()
+def main() -> None:
+    application = Application.builder().token("7390881267:AAG1t89iR5VM9j1TAHBXieyWu9S4nvtQDzg").build()
+    
+    start_handler = CommandHandler("start", start)
+    application.add_handler(start_handler)
 
-if __name__ == '__main__':
+    application.run_polling()
+
+if __name__ == "__main__":
     main()
