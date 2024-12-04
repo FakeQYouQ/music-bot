96aa9a1210602cf48ee539517237a1bb1104188c
FakeQYouQ <fakeyou@internet.ru>
2024-12-04 21:44:24 +0300

    Добавлен основной код для бота и интерфейса

--- bot.py
+++ bot.py
@@ -1,30 +1,23 @@
 import logging
-from aiogram import Bot, Dispatcher, types
-from aiogram.types import ParseMode
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode  # Обновлено для aiogram 3.x
+from aiogram.types import Message
 from aiogram.utils import executor
-import os
 
 API_TOKEN = '7390881267:AAG1t89iR5VM9j1TAHBXieyWu9S4nvtQDzg'
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+bot = Bot(token=API_TOKEN, parse_mode=ParseMode.HTML)
+dp = Dispatcher()
 
-# Обработчик команды /start
-@dp.message_handler(commands=['start'])
-async def send_welcome(message: types.Message):
-    await message.reply("Добро пожаловать в музыкального бота! Отправьте команду /play, чтобы начать.")
+@dp.message(commands=['start'])
+async def send_welcome(message: Message):
+    await message.answer("Добро пожаловать в музыкального бота! Отправьте команду /play, чтобы начать.")
 
-# Обработчик команды /play
-@dp.message_handler(commands=['play'])
-async def play_music(message: types.Message):
-    await message.reply("Вот ваш трек: /music_link")
-
-# Отправка ссылки на трек
-@dp.message_handler(commands=['music_link'])
-async def send_music_link(message: types.Message):
-    await message.reply("Трек доступен по следующей ссылке: http://your_server_address/tracks/aikko-prinjat-sebja.mp3")
+@dp.message(commands=['play'])
+async def play_music(message: Message):
+    await message.answer("Вот ваш трек: /music_link")
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    executor.run(dp)
