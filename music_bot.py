931ffabad490eb21402b547d7c67ed0aaad5e1e7
FakeQYouQ <fakeyou@internet.ru>
2024-12-04 04:07:51 +0300

    Add music_bot.py

---
+++ music_bot.py
@@ -0,0 +1,27 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+# Токен вашего бота
+TOKEN = '8140337979:AAFWnIwvNAWqB2Q_jog6ryWhpIGVr2KY0pc'
+
+# Функция, которая будет отправлять сообщение пользователю с кнопкой
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    keyboard = [
+        [InlineKeyboardButton("Open Music App", web_app=WebAppInfo(url="https://fakeqyouq.github.io/music-bot/"))]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text('Добро пожаловать в музыкальную платформу!', reply_markup=reply_markup)
+
+# Основная функция для запуска бота
+def main() -> None:
+    application = Application.builder().token(TOKEN).build()
+
+    # Обработчик команды /start
+    application.add_handler(CommandHandler("start", start))
+
+    # Запуск бота
+    application.run_polling()
+
+if __name__ == '__main__':
+    main()
